cf4e584e7e281c22804e84c30aaabede03f7c4a9
Kevin McHale <kevin@ubuntu.(none)>
2013-03-02 01:09:40 -0500

    first commit

---
+++ tictoc.h
@@ -0,0 +1,52 @@
+#include <time.h>
+#include <exception>
+
+// simple nested stack-based inline high-precision timer class
+// must link with -lrt
+// throws exceptions when used improperly
+
+#define MAX_CLOCKS 16
+
+class clock_overflow_exception : public std::exception
+{
+    virtual const char* what() const throw() {
+        return "Exceeded MAX_CLOCKS!\n";
+    }
+} tictoc_maxclock_ex;
+
+class clock_underflow_exception : public std::exception
+{
+    virtual const char* what() const throw() {
+        return "No clocks active\n";
+    }
+} tictoc_underflow_ex;
+
+
+class tictoc {
+    private:
+        struct timespec clocks[MAX_CLOCKS];
+        int depth;
+    public:
+        tictoc() : depth(0)
+        {}
+
+        inline void tic(void) {
+            if(depth == MAX_CLOCKS)
+                throw(tictoc_maxclock_ex);
+
+            clock_gettime(CLOCK_MONOTONIC_RAW, &clocks[depth++]);
+        }
+
+        inline double toc(void) {
+            struct timespec final;
+            clock_gettime(CLOCK_MONOTONIC_RAW, &final);
+
+            if(!depth)
+                throw(tictoc_underflow_ex);
+
+            depth--;
+            double ret = 1e-9*(final.tv_nsec-clocks[depth].tv_nsec)
+                        + (final.tv_sec-clocks[depth].tv_sec);
+            return ret;
+        }
+};
---
+++ tictoc_test.cpp
@@ -0,0 +1,22 @@
+#include "tictoc.h"
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(void)
+{
+    tictoc x;
+
+    x.tic();
+    usleep(1000000);
+    x.tic();
+    usleep(1000000);
+    double y1 = x.toc();
+    double y2 = x.toc();
+
+    cout << "2000000 usec => " << y1 << " sec" << endl;
+    cout << "1000000 usec => " << y2 << " sec" << endl;
+
+    return 0;
+}
